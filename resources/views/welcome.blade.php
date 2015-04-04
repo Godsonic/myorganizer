4b69b151ce577de5092f00cf6f8c37a7165eab9b
Godsonic <ramone.biduaya@gmail.com>
2015-04-04 02:09:39 +0000

    modify home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,23 +5,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
-    <title>Home | myOrganizer</title>
-    <link href="/css/bootstrap.min.css" rel="stylesheet">
-    <link href="/css/font-awesome.min.css" rel="stylesheet">
-    <link href="/css/animate.min.css" rel="stylesheet"> 
-    <link href="/css/lightbox.css" rel="stylesheet"> 
-	<link href="/css/main.css" rel="stylesheet">
-	<link href="/css/responsive.css" rel="stylesheet">
+    <title>Home | Triangle</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/animate.min.css" rel="stylesheet"> 
+    <link href="css/lightbox.css" rel="stylesheet"> 
+	<link href="css/main.css" rel="stylesheet">
+	<link href="css/responsive.css" rel="stylesheet">
 
     <!--[if lt IE 9]>
 	    <script src="js/html5shiv.js"></script>
 	    <script src="js/respond.min.js"></script>
     <![endif]-->       
-    <link rel="shortcut icon" href="/images/ico/favicon.ico">
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/images/ico/apple-touch-icon-144-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/images/ico/apple-touch-icon-114-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/images/ico/apple-touch-icon-72-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" href="/images/ico/apple-touch-icon-57-precomposed.png">
+    <link rel="shortcut icon" href="images/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="images/ico/apple-touch-icon-57-precomposed.png">
 </head><!--/head-->
 
 <body>
@@ -38,17 +38,17 @@
                     </button>
 
                     <a class="navbar-brand" href="index.html">
-                    	<h1><img src="/images/logo.png" alt="logo"></h1>
+                    	<h1><img src="images/logo.png" alt="logo"></h1>
                     </a>
                     
                 </div>
                 <div class="collapse navbar-collapse">
                     <ul class="nav navbar-nav navbar-right">
                         <li class="active"><a href="index.html">Home</a></li>
-                        <li class=""><a href="#">About us</a></li>                  
-                        <li class=""><a href="#">Team</a></li>
-                        <li class=""><a href="#">Contact us</a></li>                         
-                        <li><a href="#">Sign in</a></li>                    
+                        <li class="dropdown"><a href="#">About us</a></li>                    
+                        <li class="dropdown"><a href="blog.html">Team</a></li>
+                        <li class="dropdown"><a href="portfolio.html">Contact us</a></li>                         
+                        <li><a href="shortcodes.html ">Sign in</a></li>                    
                     </ul>
                 </div>
                 <div class="search">
@@ -58,7 +58,7 @@
                             <input type="text" class="search-form" autocomplete="off" placeholder="Search">
                         </div>
                     </form>
-                </div>
+                </div><hr>
             </div>
         </div>
     </header>
@@ -66,20 +66,19 @@
 
     <section id="home-slider">
         <div class="container">
-            <div class="main-slider">
-                <div class="slide-text">
-                    <h1>We Are Creative Nerds</h1>
-                    <p>Boudin doner frankfurter pig. Cow shank bresaola pork loin tri-tip tongue venison pork belly meatloaf short loin landjaeger biltong beef ribs shankle chicken andouille.</p>
-                    <a href="#" class="btn btn-common">SIGN UP</a>
+            <div class="row">
+                <div class="main-slider">
+                    <div class="slide-text">
+                        <h1>We Are Creative Nerds</h1>
+                        <p>Boudin doner frankfurter pig. Cow shank bresaola pork loin tri-tip tongue venison pork belly meatloaf short loin landjaeger biltong beef ribs shankle chicken andouille.</p>
+                        <a href="#" class="btn btn-common">SIGN UP</a>
+                    </div>
+                    <img src="images/home/slider/hill.png" class="slider-hill" alt="slider image">
+                    <img src="images/home/slider/house.png" class="slider-house" alt="slider image">
+                    <img src="images/home/slider/sun.png" class="slider-sun" alt="slider image">
+                    <img src="images/home/slider/birds1.png" class="slider-birds1" alt="slider image">
+                    <img src="images/home/slider/birds2.png" class="slider-birds2" alt="slider image">
                 </div>
-                <img src="/images/home/slider/slide1/house.png" class="img-responsive slider-house" alt="slider image">
-                <img src="/images/home/slider/slide1/circle1.png" class="slider-circle1" alt="slider image">
-                <img src="/images/home/slider/slide1/circle2.png" class="slider-circle2" alt="slider image">
-                <img src="/images/home/slider/slide1/cloud1.png" class="slider-cloud1" alt="slider image">
-                <img src="/images/home/slider/slide1/cloud2.png" class="slider-cloud2" alt="slider image">
-                <img src="/images/home/slider/slide1/cloud3.png" class="slider-cloud3" alt="slider image">
-                <img src="/images/home/slider/slide1/sun.png" class="slider-sun" alt="slider image">
-                <img src="/images/home/cycle.png" class="slider-cycle" alt="">
             </div>
         </div>
         <div class="preloader"><i class="fa fa-sun-o fa-spin"></i></div>
@@ -92,7 +91,7 @@
                 <div class="col-sm-4 text-center padding wow fadeIn" data-wow-duration="1000ms" data-wow-delay="300ms">
                     <div class="single-service">
                         <div class="wow scaleIn" data-wow-duration="500ms" data-wow-delay="300ms">
-                            <img src="/images/home/icon1.png" alt="">
+                            <img src="images/home/icon1.png" alt="">
                         </div>
                         <h2>Incredibly Responsive</h2>
                         <p>Ground round tenderloin flank shank ribeye. Hamkevin meatball swine. Cow shankle beef sirloin chicken ground round.</p>
@@ -101,7 +100,7 @@
                 <div class="col-sm-4 text-center padding wow fadeIn" data-wow-duration="1000ms" data-wow-delay="600ms">
                     <div class="single-service">
                         <div class="wow scaleIn" data-wow-duration="500ms" data-wow-delay="600ms">
-                            <img src="/images/home/icon2.png" alt="">
+                            <img src="images/home/icon2.png" alt="">
                         </div>
                         <h2>Superior Typography</h2>
                         <p>Hamburger ribeye drumstick turkey, strip steak sausage ground round shank pastrami beef brisket pancetta venison.</p>
@@ -121,20 +120,13 @@
     </section>
     <!--/#services-->
 
-    <!--/#action-->
-
-
     <footer id="footer">
         <div class="container">
             <div class="row">
-                <div class="col-sm-12 text-center bottom-separator">
-                    <img src="/images/home/under.png" class="img-responsive inline" alt="">
-                </div>
-
                 <div class="col-sm-12">
                     <div class="copyright-text text-center">
-                        <p>&copy; Your Company 2014. All Rights Reserved.</p>
-                        <p>Designed by <a target="_blank" href="http://www.themeum.com">Themeum</a></p>
+                        <p style="color:#000;">&copy; myOrganizer 2015. All Rights Reserved.</p>
+                        <p style="color:#000;">Designed by <a target="_blank" href="http://www.themeum.com">Ramone B</a></p>
                     </div>
                 </div>
             </div>
@@ -142,10 +134,10 @@
     </footer>
     <!--/#footer-->
 
-    <script type="text/javascript" src="/js/jquery.js"></script>
-    <script type="text/javascript" src="/js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="/js/lightbox.min.js"></script>
-    <script type="text/javascript" src="/js/wow.min.js"></script>
-    <script type="text/javascript" src="/js/main.js"></script>   
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/lightbox.min.js"></script>
+    <script type="text/javascript" src="js/wow.min.js"></script>
+    <script type="text/javascript" src="js/main.js"></script>   
 </body>
 </html>
