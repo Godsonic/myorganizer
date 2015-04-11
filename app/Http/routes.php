fc6fb25dac8f0428091b8970e887c701d2e3ae38
Godsonic <ramone.biduaya@gmail.com>
2015-04-11 15:53:44 +0000

    modify homeController

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,7 +15,7 @@ Route::get('/', 'WelcomeController@index');
 
 Route::get('dashboard', 'WelcomeController@dashboard');
 
-//Route::get('home', 'HomeController@index');
+Route::get('home', 'HomeController@index');
 
 Route::controllers([
 	'auth' => 'Auth\AuthController',
